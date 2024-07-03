b0652e1bf8105b7feedd9fb02f6481bf937ff5e2
Leonardo Perez <leperez@google.com>
2024-07-03 18:27:04 -0500

    remove changes on version.php file

--- version.php
+++ version.php
@@ -30,10 +30,10 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_bambuco';
 
 // This is the version of the plugin.
-$plugin->version = 2024062801;
+$plugin->version = 2023012912;
 
 // This is the named version.
-$plugin->release = '4.1.13';
+$plugin->release = '4.1.12';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
